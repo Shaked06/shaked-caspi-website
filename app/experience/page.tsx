82883110fc37ba4bbaea2f2e3d569a66eb5e43a0
Shaked Caspi <caspi5032@gmail.com>
2023-12-27 14:22:51 +0200

    add files

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,19 +1,17 @@
-"use client";
 import Block from "@/app/ui/experience/block";
 import { getExperienceData } from "@/app/lib/resume";
 import { Works } from "../lib/definitions";
-// import { motion } from "framer-motion";
 
 export default async function Page(props: { experienceData: Works }) {
-  // const experienceData: Works = await getExperienceData();
+  const experienceData: Works = await getExperienceData();
   try {
     return (
       <div className="flex flex-col justify-center grow gap-6 px-4 md:py-4 md:px-8">
         <div className="flex flex-col justify-center grow gap-6 px-6 md:py-10 md:px-8">
           <div className="flex h-full flex-col gap-8 px-3 py-4 md:px-2 tracking-[0.25em]">
-            {/* {Object.entries(experienceData.works).map(([key, data]) => (
+            {Object.entries(experienceData.works).map(([key, data]) => (
               <Block key={key} props={data}></Block>
-            ))} */}
+            ))}
           </div>
         </div>
       </div>
