bae00ff384423a335abcf34e3c00d382d4db533a
Shaked Caspi <caspi5032@gmail.com>
2023-12-27 21:18:07 +0200

    add files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { noto_jp } from "./ui/fonts";
 import "./globals.css";
 import clsx from "clsx";
-import SideNav from "./ui/sidenav";
+import SideNav from "@/app/ui/sidenav";
 
 export const metadata: Metadata = {
   title: "SHAKED CASPI",
@@ -18,13 +18,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={clsx(
-          "bg-gradient-to-b from-darkBlue200 to-darkBlue100 min-h-full",
+          "bg-gradient-to-b from-darkBlue200 to-darkBlue100 h-screen bg-no-repeat bg-darkBlue100",
           `${noto_jp.className} antialiased`
         )}
       >
-        <main className="flex md:min-h-screen flex-col p-2 md:p-6">
-          <div className="mt-4 flex flex-col grow gap-2 md:gap-8 md:flex-row">
-            <div className="flex flex-col justify-center grow gap-6 px-6 md:py-10 md:px-8 w-1/3">
+        <main className="flex flex-col p-2 md:p-6">
+          <div className="mt-4 flex flex-col gap-2 md:gap-8 md:flex-row">
+            <div className="flex flex-col grow justify-center gap-6 md:py-10 md:px-8">
               <SideNav />
             </div>
             <div className="w-2/3">{children}</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import Link from "next/link";
-import Image from "next/image";
-import SideNav from "./ui/sidenav";
-import { noto_jp } from "./ui/fonts";
-
 export default function Home() {
   return <p>TEST</p>;
 }
